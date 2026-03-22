ddb25ba51bc9b3106ccadcf5bf9365574b328866
HoangHuy2501 <huy04.developer@gmail.com>
2026-03-22 11:27:12 +0700

    cors

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@
 var cors=require('cors');
 module.exports=cors({
   // origin: '*', // hoặc cụ thể: http://localhost:3000
-   origin: ['http://localhost:5173'],
+   origin: ['http://localhost:5173', "https://chinhchinh127.github.io/KLTN-FWMS/"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With',],
 //   credentials: true // Cho phép gửi cookie
